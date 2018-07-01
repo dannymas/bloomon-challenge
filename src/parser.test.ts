cc0dafd6fcbbd41dec486bf3c5fe68021712e269
phra <greensoncio@gmail.com>
2018-07-01 17:13:29 +0200

    test: implement parser tests

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -8,11 +8,16 @@ describe('parser', () => {
       expect(parser.parseBouquetSpecLine).not.toThrow()
     })
 
+    it('throws error on wrong bouquet spec', () => {
+      const wrongSpec = 'X'
+      expect(() => parser.parseBouquetSpecLine(wrongSpec)).toThrow()
+    })
+
     it('parses line and returns correct bouquet spec', () => {
       const bouquetSpecLine = 'BL15b1c'
       const bouquetSpec = parser.parseBouquetSpecLine(bouquetSpecLine)
       expect(bouquetSpec).toEqual(
-        new BouquetSpec('B', 'L', { b: 15, c: 1 })
+        new BouquetSpec(bouquetSpecLine, 'B', 'L', { b: 15, c: 1 }),
       )
     })
   })
@@ -22,7 +27,12 @@ describe('parser', () => {
       expect(parser.parseFlowerLine).not.toThrow()
     })
 
-    it('parses line and returns correct flower', () => {
+    it('throws error on wrong flower spec', () => {
+      const wrongSpec = 'X'
+      expect(() => parser.parseFlowerLine(wrongSpec)).toThrow()
+    })
+
+    it('parses line and returns correct flower spec', () => {
       const flowerLine = 'cS'
       const flower = parser.parseFlowerLine(flowerLine)
       expect(flower).toEqual(new Flower('c', 'S'))
